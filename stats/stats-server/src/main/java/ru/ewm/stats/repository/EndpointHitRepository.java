ef3776bb8cde47c82441249ef2e91d2117cccef6
alekseyK <lex4509@yandex.ru>
2023-02-28 17:45:57 +0300

    fix: Перенести запросы в репозиторий

--- stats/stats-server/src/main/java/ru/ewm/stats/repository/EndpointHitRepository.java
+++ stats/stats-server/src/main/java/ru/ewm/stats/repository/EndpointHitRepository.java
@@ -9,19 +9,21 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> {
-    @Query("SELECT new dto.ViewStatsDto(e.app, e.uri, COUNT(distinct e.ip)) " +
-            " FROM EndpointHit AS e" +
-            " WHERE e.timestamp >= :start" +
-            " AND e.timestamp <= :end" +
-            " AND e.uri IN :uris" +
-            " GROUP BY e.app, e.uri")
+//    @Query("SELECT new dto.ViewStatsDto(e.app, e.uri, COUNT(distinct e.ip)) " +
+//            " FROM EndpointHit AS e" +
+//            " WHERE e.timestamp >= :start" +
+//            " AND e.timestamp <= :end" +
+//            " AND e.uri IN :uris" +
+//            " GROUP BY e.app, e.uri")
+    @Query(nativeQuery = true, name = "ViewStatsDtos")
     List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new dto.ViewStatsDto(e.app, e.uri, COUNT(distinct e.ip)) " +
-            " FROM EndpointHit AS e" +
-            " WHERE e.timestamp >= :start" +
-            " AND e.timestamp <= :end" +
-            " AND e.uri IN :uris" +
-            " GROUP BY e.app, e.uri")
+//    @Query("SELECT new dto.ViewStatsDto(e.app, e.uri, COUNT(distinct e.ip)) " +
+//            " FROM EndpointHit AS e" +
+//            " WHERE e.timestamp >= :start" +
+//            " AND e.timestamp <= :end" +
+//            " AND e.uri IN :uris" +
+//            " GROUP BY e.app, e.uri")
+    @Query(nativeQuery = true, name = "ViewStatsDtosUniqueIps")
     List<ViewStatsDto> getStatsUniqueIps(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
