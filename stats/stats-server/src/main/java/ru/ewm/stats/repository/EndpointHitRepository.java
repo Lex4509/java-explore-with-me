6080814d11975a627eb5e140c18ee3994fdc6369
alekseyK <lex4509@yandex.ru>
2023-02-28 17:49:26 +0300

    fix: Перенести запросы в репозиторий

--- stats/stats-server/src/main/java/ru/ewm/stats/repository/EndpointHitRepository.java
+++ stats/stats-server/src/main/java/ru/ewm/stats/repository/EndpointHitRepository.java
@@ -5,9 +5,38 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.ewm.stats.model.EndpointHit;
 
+import javax.persistence.ColumnResult;
+import javax.persistence.ConstructorResult;
+import javax.persistence.NamedNativeQuery;
+import javax.persistence.SqlResultSetMapping;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@NamedNativeQuery(name = "ViewStatsDtos",
+        query = "select h.app, h.uri, count(h.ip) as ehits from hits as h " +
+                "where h.timestamp > ?1 " +
+                "and h.timestamp < ?2 " +
+                "and h.uri in ?3 " +
+                "group by h.app, h.uri " +
+                "order by ehits desc", resultSetMapping = "ViewStatsDtoMapping")
+@NamedNativeQuery(name = "ViewStatsDtosUniqueIps",
+        query = "select h.app, h.uri, count(distinct (h.ip)) as ehits from hits as h " +
+                "where h.timestamp > ?1 " +
+                "and h.timestamp < ?2 " +
+                "and h.uri in ?3 " +
+                "group by h.app, h.uri " +
+                "order by ehits desc", resultSetMapping = "ViewStatsDtoMapping")
+@SqlResultSetMapping(name = "ViewStatsDtoMapping",
+        classes = {
+                @ConstructorResult(
+                        columns = {
+                                @ColumnResult(name = "app", type = String.class),
+                                @ColumnResult(name = "uri", type = String.class),
+                                @ColumnResult(name = "ehits", type = Long.class)
+                        },
+                        targetClass = ViewStatsDto.class
+                )}
+)
 public interface EndpointHitRepository extends JpaRepository<EndpointHit, Long> {
 //    @Query("SELECT new dto.ViewStatsDto(e.app, e.uri, COUNT(distinct e.ip)) " +
 //            " FROM EndpointHit AS e" +
