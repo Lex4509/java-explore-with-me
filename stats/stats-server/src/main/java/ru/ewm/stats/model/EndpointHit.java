d7e5bdbbfd1c3989c04885d5d6b0501ac6c57d3f
alekseyK <lex4509@yandex.ru>
2023-02-28 23:14:56 +0300

    fix: Перенести запросы в репозиторий

--- stats/stats-server/src/main/java/ru/ewm/stats/model/EndpointHit.java
+++ stats/stats-server/src/main/java/ru/ewm/stats/model/EndpointHit.java
@@ -1,5 +1,6 @@
 package ru.ewm.stats.model;
 
+import dto.ViewStatsDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,31 +15,31 @@ import java.time.LocalDateTime;
 @Setter
 @Entity
 @Table(name = "hits")
-//@NamedNativeQuery(name = "ViewStatsDtos",
-//        query = "select h.app, h.uri, count(h.ip) as ehits from hits as h " +
-//        "where h.timestamp > ?1 " +
-//        "and h.timestamp < ?2 " +
-//        "and h.uri in ?3 " +
-//        "group by h.app, h.uri " +
-//        "order by ehits desc", resultSetMapping = "ViewStatsDtoMapping")
-//@NamedNativeQuery(name = "ViewStatsDtosUniqueIps",
-//        query = "select h.app, h.uri, count(distinct (h.ip)) as ehits from hits as h " +
-//        "where h.timestamp > ?1 " +
-//        "and h.timestamp < ?2 " +
-//        "and h.uri in ?3 " +
-//        "group by h.app, h.uri " +
-//        "order by ehits desc", resultSetMapping = "ViewStatsDtoMapping")
-//@SqlResultSetMapping(name = "ViewStatsDtoMapping",
-//        classes = {
-//                @ConstructorResult(
-//                        columns = {
-//                                @ColumnResult(name = "app", type = String.class),
-//                                @ColumnResult(name = "uri", type = String.class),
-//                                @ColumnResult(name = "ehits", type = Long.class)
-//                        },
-//                        targetClass = ViewStatsDto.class
-//                )}
-//)
+@NamedNativeQuery(name = "ViewStatsDtos",
+        query = "select h.app, h.uri, count(h.ip) as ehits from hits as h " +
+        "where h.timestamp > ?1 " +
+        "and h.timestamp < ?2 " +
+        "and h.uri in ?3 " +
+        "group by h.app, h.uri " +
+        "order by ehits desc", resultSetMapping = "ViewStatsDtoMapping")
+@NamedNativeQuery(name = "ViewStatsDtosUniqueIps",
+        query = "select h.app, h.uri, count(distinct (h.ip)) as ehits from hits as h " +
+        "where h.timestamp > ?1 " +
+        "and h.timestamp < ?2 " +
+        "and h.uri in ?3 " +
+        "group by h.app, h.uri " +
+        "order by ehits desc", resultSetMapping = "ViewStatsDtoMapping")
+@SqlResultSetMapping(name = "ViewStatsDtoMapping",
+        classes = {
+                @ConstructorResult(
+                        columns = {
+                                @ColumnResult(name = "app", type = String.class),
+                                @ColumnResult(name = "uri", type = String.class),
+                                @ColumnResult(name = "ehits", type = Long.class)
+                        },
+                        targetClass = ViewStatsDto.class
+                )}
+)
 public class EndpointHit {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
